c09edf4207167326eae6b93e0f34132f23d355cb
Junser Hwang <hzser123@gmail.com>
2023-09-01 22:48:42 +0900

    [FIX] 초기데이터 삽입 기능 추가

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import shutil
 from multiprocessing import Process
 
 from src.core.worker import run_workers
@@ -7,11 +9,25 @@ from src.helpers.config import config
 from src.helpers.log import setup_logging
 from src.model.model import init_db
 from src.model.template import refresh_template_list, get
+from src.helpers.path import get_absolute_path
 
 setup_logging()
+
+
+def init_data():
+    data_path = get_absolute_path(['data', 'template'])
+    bootstrap_path = get_absolute_path(['bootstrap', 'template', 'plain_text.html'])
+
+    if not os.path.exists(data_path):
+        logging.info("Initialize data directory")
+        os.makedirs(data_path)
+        shutil.copy(bootstrap_path, data_path)
+
+
 if __name__ == '__main__':
 
     init_db()
+    init_data()
     result = refresh_template_list()
     if not result:
         exit(1)
